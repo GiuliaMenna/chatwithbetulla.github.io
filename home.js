1622be1e8a627ed812fd2bc59eeb24c69b6741a5
GiuliaMenna <72873975+GiuliaMenna@users.noreply.github.com>
2022-01-21 11:07:48 +0100

    news

--- home.js
+++ home.js
@@ -57,3 +57,16 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+$(document).ready(function() {
+
+    window.addEventListener('dfMessengerLoaded', function (event) {
+        $r1 = document.querySelector("df-messenger");
+        $r2 = $r1.shadowRoot.querySelector("df-messenger-chat");
+        $r3 = $r2.shadowRoot.querySelector("df-messenger-user-input"); //for other mods
+
+        var sheet = new CSSStyleSheet;
+        sheet.replaceSync( `div.chat-wrapper[opened="true"] { height: 400px }`);
+        $r2.shadowRoot.adoptedStyleSheets = [ sheet ];
+
+    });
+});
